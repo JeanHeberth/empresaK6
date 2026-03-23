b45d344ffd31ed055612ca494f1a1eadb6c86bd1
jeanheberth <jeanheberth19@gmail.com>
2026-03-23 16:44:10 -0300

    adf

--- teste-get.js
+++ teste-get.js
@@ -11,6 +11,11 @@ export const options = {
         { duration: '10s', target: 600 },
         { duration: '10s', target: 0 },
     ],
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<800'],
+        checks: ['rate>0.99'],
+    },
 };
 
 export default function () {
